1c1529edad935a3abe6615c9193cb07842091594
Isaac Lubanko <isaac.lubanko@greenphire.com>
2019-02-18 17:06:08 -0500

    starting on building out the character sheet

--- character/views.py
+++ character/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from . import models
 # Create your views here.
+
+def sheet(request, id):
+    sheet = Character.objects.get(id=id)
+    context = {
+        'character': sheet
+    }
+    return('charsheet.html', context)
