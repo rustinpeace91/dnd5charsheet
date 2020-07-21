01e40a0c0847ec96e08d676dd0f02e1a0c726fbb
isaac <isaaclubanko@comcast.net>
2020-07-20 22:59:22 -0400

    configure bootstrap template

--- charsheet/settings.py
+++ charsheet/settings.py
@@ -127,7 +127,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = ''
 STATIC_URL = '/static/'
+STATICFILES_DIRS = ( os.path.join('static'), )
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- charsheet/urls.py
+++ charsheet/urls.py
@@ -23,6 +23,4 @@ urlpatterns = [
     path('', include('home.urls')),
     path('character/', include('character.urls')),
     path('admin/', admin.site.urls)
-]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
